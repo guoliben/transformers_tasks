689e6436751fb0000f9d9b36b75804335ab04796
郭立本 <guoliben@kylinos.cn>
2024-09-04 09:08:33 +0800

    update

--- answer_generation/train.sh
+++ answer_generation/train.sh
@@ -1,3 +1,6 @@
+export HF_ENDPOINT=https://hf-mirror.com
+export PYTORCH_ENABLE_MPS_FALLBACK=1
+
 python train.py \
     --pretrained_model "uer/t5-base-chinese-cluecorpussmall" \
     --save_dir "checkpoints/DuReaderQG" \
@@ -5,11 +8,15 @@ python train.py \
     --dev_path "data/DuReaderQG/dev.json" \
     --img_log_dir "logs/DuReaderQG" \
     --img_log_name "T5-Base-Chinese" \
-    --batch_size 32 \
-    --max_source_seq_len 256 \
-    --max_target_seq_len 32 \
+    --batch_size 4 \
+    --max_source_seq_len 20 \
+    --max_target_seq_len 8 \
     --learning_rate 5e-5 \
-    --num_train_epochs 50 \
-    --logging_steps 10 \
-    --valid_steps 500 \
-    --device cuda:0
+    --num_train_epochs 30 \
+    --logging_steps 100 \
+    --valid_steps 50 \
+    --device "mps:0"
+
+#mps:0
+#cpu
+#cuda:0
